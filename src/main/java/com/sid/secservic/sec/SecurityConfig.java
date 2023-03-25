5150bd90258f78baa137511c8b7088268a218e18
aadlani mohammed <aadlan9@gmail.com>
2023-03-25 14:45:13 +0000

    Authentification

--- src/main/java/com/sid/secservic/sec/SecurityConfig.java
+++ src/main/java/com/sid/secservic/sec/SecurityConfig.java
@@ -1,20 +1,57 @@
 package com.sid.secservic.sec;
 
+import com.sid.secservic.sec.entity.AppUser;
+import com.sid.secservic.sec.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                AppUser appUser = accountService.loadUserByUsername(username);
+
+                Collection<GrantedAuthority> authorities = new ArrayList<>();
+                appUser.getAppRoles().forEach(role ->{
+                    authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+                });
+                return new User(appUser.getUsername(),appUser.getPassword(),authorities);
+            }
+        });
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().disable(); //Stateful : Sans mémoire  (session gérer à la côté de serveur)
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.headers().frameOptions().disable();
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        //http.formLogin();
+        http.authorizeRequests().anyRequest().authenticated();
 
         return http.build();
     }
